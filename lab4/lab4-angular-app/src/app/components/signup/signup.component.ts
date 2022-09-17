d25aadc5618e0ee186b1e97c568a7ba76027eb23
Natchaya Phongkuson <tungpar7401@hotmail.com>
2022-09-17 13:44:38 +0700

    อัพงาน

---
+++ lab4/lab4-angular-app/src/app/components/signup/signup.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+
+export class SignupComponent implements OnInit {
+
+  profileForm = new FormGroup({
+    title: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''), 
+    sex: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    studentnumber: new FormControl('',[Validators.required, Validators.pattern('B[0-9]{7}')]),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl(''),
+    }),
+    aliases: new FormArray([
+      new FormControl('')
+    ])
+  });
+
+  get email() { return this.profileForm.get('email');}
+  get studentnumber() { return this.profileForm.get('studentnumber');}
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addAlias() {
+    this.aliases.push(new FormControl(''));
+  }
+
+}
